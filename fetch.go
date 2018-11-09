eb6e137c61ff15698971f6aa63107c50f83b63f7
agfy <agfy1@mail.ru>
2018-11-09 16:17:25 +0300

    first commit

---
+++ fetch.go
@@ -0,0 +1,28 @@
+package main
+
+import ( 
+	"fmt" 
+	"net/http" 
+	"os"
+	"io"
+	"strings"
+)
+
+func main() { 
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		} 
+		resp, err := http.Get(url) 
+		if err != nil { 
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err) 
+			os.Exit(1) 
+		} 
+
+		_, err = io.Copy(os.Stdout, resp.Body) 
+		if err != nil { 
+			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err) 
+			os.Exit(1) 
+		} 
+	} 
+}
